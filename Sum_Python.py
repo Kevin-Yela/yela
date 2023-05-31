8d51a3614618b15e25318aedbe89e1aef0a1aa65
Kevin-Yela <133878818+Kevin-Yela@users.noreply.github.com>
2023-05-31 08:08:46 -0500

    Update Sum_Python.py

--- Sum_Python.py
+++ Sum_Python.py
@@ -13,3 +13,8 @@ resultado_resta = resta(numero1, numero2)
 
 print("La suma de", numero1, "y", numero2, "es:", resultado_suma)
 print("La resta de", numero1, "y", numero2, "es:", resultado_resta)
+
+ #Yela
+    #Idolo
+    #IEM_ESCUELA_NORMAL_PASTO
+    #11-1
